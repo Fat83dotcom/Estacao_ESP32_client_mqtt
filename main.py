210c2fba0d507d099cabf55b48b4f292ea4fb0a8
fat83dotcom <fernando.quimic@gmail.com>
2023-10-29 19:17:58 -0300

    Modificações que não serão mais usadas.

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import time
 
 broker = 'broker.hivemq.com'
 port = 1883
-topic = 'ESP32_Send_Information'
-client_id = 'ESP32_MQTT_BRANISTORMTECH'
+topic_sub_temp = 'ESP32_Sensors_BME280_TEMP'
+topic_sub_press = 'ESP32_Sensors_BME280_PRESS'
+topic_sub_humi = 'ESP32_Sensors_BME280_HUMI'
+topic_pub = 'ESP32_Receive_Information'
+client_id = 'Python'
 
 
 def connect_mqtt():
@@ -55,13 +58,13 @@ def publish(client):
     while True:
         time.sleep(1)
         msg = f"messages: {msg_count}"
-        result = client.publish(topic, msg)
+        result = client.publish(topic_pub, msg)
         # result: [0, 1]
         status = result[0]
         if status == 0:
-            print(f"Send `{msg}` to topic `{topic}`")
+            print(f"Send `{msg}` to topic `{topic_pub}`")
         else:
-            print(f"Failed to send message to topic {topic}")
+            print(f"Failed to send message to topic {topic_pub}")
         msg_count += 1
         if msg_count > 5:
             break
@@ -69,16 +72,21 @@ def publish(client):
 
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
-        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+        print(
+            f"Received {msg.payload.decode('utf-8')} from {msg.topic} topic"
+        )
 
-    client.subscribe(topic)
+    client.subscribe(topic_sub_temp)
+    client.subscribe(topic_sub_press)
+    client.subscribe(topic_sub_humi)
     client.on_message = on_message
 
 
 def run():
     client = connect_mqtt()
-    subscribe(client)
-    client.loop_forever()
+    while 1:
+        subscribe(client)
+        client.loop_forever()
 
 
 if __name__ == '__main__':
