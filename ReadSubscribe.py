95be0f50d1f4998e50e0eff85abbf2d73c42e32c
fat83dotcom <fernando.quimic@gmail.com>
2023-11-05 20:32:45 -0300

    Implementando a classe que vai gerenciar os sensores no banco de dados.

--- ReadSubscribe.py
+++ ReadSubscribe.py
@@ -1,5 +1,27 @@
 import paho.mqtt.client as mqtt
+from DataBaseManager.OperationalDataBase import Sensors
 import json
+
+
+class VerifySensors:
+    def __init__(self) -> None:
+        self.__sensorsOnDataBase: list = []
+
+    @property
+    def sensors(self):
+        return self.__sensorsOnDataBase
+
+    @sensors.setter
+    def sensors(self, value):
+        if isinstance(value, str):
+            self.__sensorsOnDataBase.append(value)
+        else:
+            raise ValueError(
+                f'Verifique a entrada mac dos sensores! -> {value}'
+            )
+
+    def searchSensors(self):
+        pass
 
 
 topic_sub = "ESP32_Sensors_BME280"
