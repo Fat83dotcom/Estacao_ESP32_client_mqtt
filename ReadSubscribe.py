93092a4d1a06666f772dc8206fe2085a2f806894
fat83dotcom <fernando.quimic@gmail.com>
2023-11-12 19:24:07 -0300

    Veirifica se os sensores que estão enviando dados ja estão na lista do banco de dados, se sim, o programa prosegue, caso contrario ele é registrado no banco e adicionado a lista.

--- ReadSubscribe.py
+++ ReadSubscribe.py
@@ -20,23 +20,62 @@ class VerifySensors:
     @sensors.setter
     def sensors(self, value):
         if isinstance(value, str):
-            self.__sensorsOnDataBase.append(value)
+            if self.__insertSensors(value):
+                self.__sensorsOnDataBase.append(value)
         else:
             raise ValueError(
                 f'Verifique a entrada mac dos sensores! -> {value}'
             )
 
-    def searchSensors(self):
-        pass
+    def __searchSensors(self):
+        result: list = self.sensorsInstace.execSelectOnTable(
+            table='sensor',
+            collCodiction='mac',
+            condiction='',
+            conditionLiteral='',
+        )
+        if result is None:
+            return []
+        else:
+            return result
+
+    def __insertSensors(self, *args) -> bool:
+        if self.sensorsInstace.execInsertTable(
+            *args,
+            table='sensor',
+            collumn=('mac',),
+        ):
+            return True
+        else:
+            return False
+
+    def getSensorMac(self) -> list:
+        if self.__sensorsOnDataBase:
+            sensorMacs: list = [macs[2] for macs in self.__sensorsOnDataBase]
+            return sensorMacs
+        else:
+            return []
 
 
 topic_sub = "ESP32_Sensors_BME280"
 
+sens = VerifySensors()
+
+receiveDataOnSensors: dict = {}
+
 
 def on_message(client, userdata, msg):
     msgDecode = str(msg.payload.decode('utf-8', 'ignore'))
-    msgJson = json.loads(msgDecode)
-    print(msgJson)
+    receiveDataOnSensors: dict = json.loads(msgDecode)
+    receiveDataOnSensors['dataHora'] = strftime(
+        '%d/%m/%Y %H:%M:%S', localtime(int(receiveDataOnSensors['dataHora']))
+    )
+    if receiveDataOnSensors['IDMac'] not in sens.getSensorMac():
+        sens.sensors = receiveDataOnSensors['IDMac']
+    else:
+        print('já tem!')
+    print(sens.sensors)
+    print(receiveDataOnSensors)
 
 
 mqttBroker = 'broker.hivemq.com'
