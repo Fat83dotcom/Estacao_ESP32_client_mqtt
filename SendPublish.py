e9bcea2430094335e4674ef0c7b88585b66817a7
fat83dotcom <fernando.quimic@gmail.com>
2023-12-19 01:24:25 -0300

    Refatoração.

--- SendPublish.py
+++ SendPublish.py
@@ -1,27 +1,10 @@
-import paho.mqtt.client as mqtt
 from daemonize import Daemonize
-from time import sleep
+from clientMQTT import Main, PlublishMQTTClient
 
+
+pubClient = PlublishMQTTClient()
+main = Main(pubClient)
 
 pid = '/tmp/SendPublish.pid'
-
-
-def main():
-    topic_pub = 'Require_Data'
-    msg = 'return'
-
-    mqttBroker = 'broker.hivemq.com'
-    port = 1883
-    client = mqtt.Client()
-    client.connect(mqttBroker, port)
-    while 1:
-        status = client.publish(topic_pub, msg)
-        if status:
-            print(msg)
-        else:
-            print('falhou msg')
-        sleep(60)
-
-
-daemon = Daemonize(app='SendPublish', pid=pid, action=main)
+daemon = Daemonize(app='SendPublish', pid=pid, action=main.run)
 daemon.start()
