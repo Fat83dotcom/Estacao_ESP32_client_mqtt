fc61bf263cc3eb8a8bb86db397c98405f6bd4bb4
fat83dotcom <fernando.quimic@gmail.com>
2024-02-03 23:04:43 -0300

    Adicionando uma linha vazia.

--- SendPublish.py
+++ SendPublish.py
@@ -4,6 +4,7 @@ from clientMQTT import Main, PlublishMQTTClient
 pubClient = PlublishMQTTClient()
 main = Main(pubClient)
 
+
 pid = '/tmp/SendPublish.pid'
 daemon = Daemonize(app='SendPublish', pid=pid, action=main.run)
 daemon.start()
