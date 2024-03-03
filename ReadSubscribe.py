8f93bc588dc8d9b8c88206e7c034b7cc59a3132b
fat83dotcom <fernando.quimic@gmail.com>
2024-03-02 21:49:20 -0300

    Passando as credenciais para a classe.

--- ReadSubscribe.py
+++ ReadSubscribe.py
@@ -1,10 +1,12 @@
 from daemonize import Daemonize
-from DataBaseManager.settings_db import banco
+from DataBaseManager.settings_db import banco, credentialBorker
 from DataBaseManager.OperationalDataBase import DataBasePostgreSQL
 from clientMQTT import Main, SubscribeMQTTClient
 
 dbPostgreSQL = DataBasePostgreSQL(banco)
-subClient = SubscribeMQTTClient(dbPostgreSQL)
+subClient = SubscribeMQTTClient(
+    dbPostgreSQL, credentialBorker['user'], credentialBorker['password']
+)
 main = Main(subClient)
 
 
